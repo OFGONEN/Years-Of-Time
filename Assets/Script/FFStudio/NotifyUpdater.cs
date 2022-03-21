e0fdc6c83908c7a8d07c28a67be4064fc414a9ea
ofgonen <ofgonen@gmail.com>
2022-03-21 22:01:07 +0300

    Implement NotifyUpdater

---
+++ Assets/Script/FFStudio/NotifyUpdater.cs
@@ -0,0 +1,38 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Sirenix.OdinInspector;
+
+namespace FFStudio
+{
+	public class NotifyUpdater<SharedDataNotifierType, SharedDataType> : MonoBehaviour
+			where SharedDataNotifierType : SharedDataNotifier<SharedDataType>
+	{
+#region Fields
+		[ BoxGroup( "Setup" ), SerializeField ] protected SharedDataNotifierType sharedDataNotifier;
+		[ BoxGroup( "Setup" ), SerializeField ] protected UnityEvent notify_event;
+#endregion
+
+#region Unity API
+		private void OnEnable()
+		{
+			sharedDataNotifier.Subscribe( OnSharedDataChange );
+		}
+
+		private void OnDisable()
+		{
+			sharedDataNotifier.Unsubscribe( OnSharedDataChange );
+		}
+#endregion
+
+#region Base Class API
+		protected virtual void OnSharedDataChange()
+		{
+			notify_event.Invoke();
+		}
+#endregion
+	}
+}

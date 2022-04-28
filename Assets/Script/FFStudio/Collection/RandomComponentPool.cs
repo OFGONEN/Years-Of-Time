87dcc1f779fde36006c3c87f55aefa90dadf48e4
ofgonen <ofgonen@gmail.com>
2022-04-29 02:31:37 +0300

    Implement RandomComponent Pool

---
+++ Assets/Script/FFStudio/Collection/RandomComponentPool.cs
@@ -0,0 +1,34 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FFStudio
+{
+	public class RandomComponentPool< T > : ScriptableObject where T: Component
+	{
+#region Fields
+        [ SerializeField ] ComponentPool< T >[] pool_component;
+#endregion
+
+#region API
+        public void InitPool( Transform parent, bool active )
+        {
+            for( var i = 0; i < pool_component.Length; i++ )
+            {
+				pool_component[ i ].InitPool( parent, active );
+			}
+        }
+        public ComponentPool< T > GetRandomPool()
+        {
+			return pool_component.ReturnRandom();
+		}
+
+        public T GetRandomEntity()
+        {
+			return pool_component.ReturnRandom().GetEntity();
+		}
+#endregion
+	}
+}

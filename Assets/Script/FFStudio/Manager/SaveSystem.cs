bb3ced013679dd463f527fc2d07dab861a50133b
ofgonen <ofgonen@gmail.com>
2022-06-13 16:07:48 +0300

    Implement SaveSystem

---
+++ Assets/Script/FFStudio/Manager/SaveSystem.cs
@@ -0,0 +1,87 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace FFStudio
+{
+	[ CreateAssetMenu( fileName = "system_save", menuName = "FF/System/Save" ) ]
+	public class SaveSystem : ScriptableObject
+	{
+#region Fields
+		[ SerializeField ] SharedStringNotifier save_string;
+
+		static SaveSystem instance;
+#endregion
+
+#region Properties
+		public static SaveSystem Instance => instance;
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+		public void Initiliaze()
+		{
+			instance  = this;
+
+			// Create folder
+			if( Directory.Exists( ExtensionMethods.SAVE_PATH ) == false )
+				Directory.CreateDirectory( ExtensionMethods.SAVE_PATH );
+		}
+
+		public void SaveOverrideWithSharedString()
+		{
+			File.WriteAllText( ExtensionMethods.SAVE_PATH + "save.txt", save_string.sharedValue );
+			FFStudio.FFLogger.Log( "Savemanager: Saved Succesfully. Data saved: " + save_string.sharedValue );
+		}
+
+		public void SaveOverride( string save )
+		{
+			File.WriteAllText( ExtensionMethods.SAVE_PATH + "save.txt", save );
+			FFStudio.FFLogger.Log( "Savemanager: Saved Succesfully. Data saved: " + save );
+		}
+
+		public string LoadSave()
+		{
+			if( File.Exists( ExtensionMethods.SAVE_PATH + "save.txt" ) == false )
+				return null;
+
+			var json = File.ReadAllText( ExtensionMethods.SAVE_PATH + "save.txt" );
+			FFStudio.FFLogger.Log( "SaveSystem: Loaded Succesfully. Data read: " + json );
+
+			return json;
+		}
+
+		public void LoadSaveToSharedString()
+		{
+			if( File.Exists( ExtensionMethods.SAVE_PATH + "save.txt" ) == false )
+				return;
+
+			save_string.SharedValue = File.ReadAllText( ExtensionMethods.SAVE_PATH + "save.txt" );
+			FFStudio.FFLogger.Log( "SaveSystem: Loaded Succesfully. Data read: " + save_string.sharedValue );
+		}
+
+		public void DeleteSave()
+		{
+			if( File.Exists( ExtensionMethods.SAVE_PATH + "save.txt" ) )
+			{
+				FFStudio.FFLogger.Log( "SaveSystem: Found save file. Deleting it." );
+				File.Delete( ExtensionMethods.SAVE_PATH + "save.txt" );
+			}
+		}
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+	}
+}

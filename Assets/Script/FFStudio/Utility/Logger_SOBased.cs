3f06919bd2daf768d3171efad6d67438964dddb4
fauder <canik.burak@gmail.com>
2022-08-04 12:48:35 +0300

    🔃 Logger (SO Based): Change .asset name & add APIs for context & UI Popup Text support

--- Assets/Script/FFStudio/Utility/Logger_SOBased.cs
+++ Assets/Script/FFStudio/Utility/Logger_SOBased.cs
@@ -1,13 +1,16 @@
 /* Created by and for usage of FF Studios (2021). */
 
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace FFStudio
 {
-    [ CreateAssetMenu( fileName = "logger_so", menuName = "FF/Utility/Logger" ) ]
+    [ CreateAssetMenu( fileName = "util_logger", menuName = "FF/Utility/Logger" ) ]
     public class Logger_SOBased : ScriptableObject
     {
 #region Fields
+        [ ShowInInspector ] Transform target_transform; // Info: Used for both context & Popup Texts.
+        [ ShowInInspector ] GameObject target_gameObject; // Info: Used for context.
 #endregion
 
 #region Properties
@@ -21,6 +24,36 @@ namespace FFStudio
         {
             FFLogger.Log( text );
         }
+        
+        public void Log_WithContext_Transform( string text )
+        {
+            FFLogger.Log( text, target_transform );
+        }
+
+		public void Log_WithContext_GameObject( string text )
+		{
+			FFLogger.Log( text, target_gameObject );
+		}
+        
+        public void SetTarget( Transform transform )
+        {
+			target_transform = transform;
+		}
+
+		public void SetTarget( GameObject gameObject )
+		{
+			target_gameObject = gameObject;
+		}
+        
+        public void Log_Popup_Transform( string text )
+        {
+			FFLogger.PopUpText( target_transform.position, text );
+		}
+
+		public void Log_Popup_GameObject( string text )
+		{
+			FFLogger.PopUpText( target_gameObject.transform.position, text );
+		}
 #endregion
 
 #region Implementation

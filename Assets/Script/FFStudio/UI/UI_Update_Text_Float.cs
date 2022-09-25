eabb69441edf70d3756990a5375b2daf0f43e356
ofgonen_desktop <ofgonen@gmail.com>
2022-09-25 18:27:30 +0300

    UI_Update_Text_Float: Use format on float value while converting to string

--- Assets/Script/FFStudio/UI/UI_Update_Text_Float.cs
+++ Assets/Script/FFStudio/UI/UI_Update_Text_Float.cs
@@ -1,8 +1,16 @@
 /* Created by and for usage of FF Studios (2021). */
 
+using UnityEngine;
+
 namespace FFStudio
 {
     public class UI_Update_Text_Float : UI_Update_Text< SharedFloatNotifier, float >
     {
+        [ SerializeField ] string format = string.Empty;
+
+		protected override void OnSharedDataChange()
+		{
+			ui_Text.text = sharedDataNotifier.SharedValue.ToString( format );
+		}
     }
 }

a16042cbe01b1d22856bcabb47ee83ff79a2943c
fauder_home <canik.burak@gmail.com>
2022-09-25 16:50:59 +0300

    New Component: Shared Progress Notifier

---
+++ Assets/Script/FFStudio/Data/Shared_Notifier/SharedProgressNotifier.cs
@@ -0,0 +1,32 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using UnityEngine;
+using FFStudio;
+
+[ CreateAssetMenu( fileName = "notif_progress_", menuName = "FF/Data/Shared/Notifier/Progress" ) ]
+public class SharedProgressNotifier : SharedFloatNotifier
+{
+#region Fields
+    float numerator, denominator;
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+    public void SetDenominator( float denominator )
+    {
+		this.denominator = denominator;
+	}
+    
+    public void SetNumerator( float numerator )
+    {
+		this.numerator = numerator;
+
+		SharedValue = numerator / denominator;
+	}
+#endregion
+
+#region Implementation
+#endregion
+}

66d4cf4d653326d2c62026a4a6f8b0c776c09783
fauder_office <canik.burak@gmail.com>
2022-11-01 12:56:45 +0300

    🔃 SharedProgressNotifier: Add editor-only warnings for denominator = 0 case & set default values
    Default values: 0/100

--- Assets/Script/FFStudio/Data/Shared_Notifier/SharedProgressNotifier.cs
+++ Assets/Script/FFStudio/Data/Shared_Notifier/SharedProgressNotifier.cs
@@ -7,21 +7,44 @@ using FFStudio;
 public class SharedProgressNotifier : SharedFloatNotifier
 {
 #region Fields
-    float numerator, denominator;
+    float numerator = 0, denominator = 100;
 #endregion
 
 #region Unity API
+	void Reset()
+	{
+		numerator = 0;
+		denominator = 100;
+	}
 #endregion
 
 #region API
     public void SetDenominator( float denominator )
     {
+#if UNITY_EDITOR
+		if( Mathf.Approximately( denominator, 0 ) )
+		{
+			FFLogger.LogWarning( name + ": Denominator can not be zero.", this );
+			denominator = 1;
+			return;
+		}
+#endif
+
 		this.denominator = denominator;
 	}
     
     public void SetNumerator( float numerator )
     {
 		this.numerator = numerator;
+
+#if UNITY_EDITOR
+		if( Mathf.Approximately( denominator, 0 ) )
+		{
+			FFLogger.LogWarning( name + ": Denominator was zero. Setting it to 1 for now. Please inspect your code.", this );
+			denominator = 1;
+			return;
+		}
+#endif
 
 		SharedValue = numerator / denominator;
 	}

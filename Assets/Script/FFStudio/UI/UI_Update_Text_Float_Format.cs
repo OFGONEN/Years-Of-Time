7ad8a21e6fde82e395d51b984b32fd27c22f67a6
fauder_home <canik.burak@gmail.com>
2022-11-16 20:07:58 +0300

    UI Update Text Float Format: Use Invariant culture while formatting
    Fixed dot/comma incompatibility.

--- Assets/Script/FFStudio/UI/UI_Update_Text_Float_Format.cs
+++ Assets/Script/FFStudio/UI/UI_Update_Text_Float_Format.cs
@@ -1,14 +1,16 @@
 /* Created by and for usage of FF Studios (2021). */
 
+using System.Globalization;
+
 namespace FFStudio
 {
     public class UI_Update_Text_Float_Format : UI_Update_Text< SharedFloatNotifier, float >
     {
         public string format = "{0:0.##}";
-        
-        protected override void OnSharedDataChange()
+
+		protected override void OnSharedDataChange()
         {
-			ui_Text.text = System.String.Format( format, sharedDataNotifier.SharedValue );
+			ui_Text.text = System.String.Format( NumberFormatInfo.InvariantInfo, format, sharedDataNotifier.SharedValue );
 		}
     }
 }

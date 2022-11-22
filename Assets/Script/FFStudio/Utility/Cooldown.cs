b225fb6d6806991e2617b7f993b08fc39068dc73
ofgonen_office <ofgonen@gmail.com>
2022-11-22 16:55:01 +0300

    Cooldown: Wrap into FFStudio namespace

--- Assets/Script/FFStudio/Utility/Cooldown.cs
+++ Assets/Script/FFStudio/Utility/Cooldown.cs
@@ -4,66 +4,69 @@ using UnityEngine;
 using FFStudio;
 using DG.Tweening;
 
-public class Cooldown
+namespace FFStudio
 {
+	public class Cooldown
+	{
 #region Fields
-    RecycledTween recycledTween_cooldown = new RecycledTween();
-    
-    bool isOngoing = false;
-    
-    TweenCallback onComplete;
+		RecycledTween recycledTween_cooldown = new RecycledTween();
 
-	bool popupTextOnComplete = false;
-	string description;
+		bool isOngoing = false;
+
+		TweenCallback onComplete;
+
+		bool popupTextOnComplete = false;
+		string description;
 #endregion
 
 #region Properties
-    public bool IsOver => !isOngoing;
-	public float ElapsedPercentage => recycledTween_cooldown.Tween.ElapsedPercentage();
+		public bool IsOver => !isOngoing;
+		public float ElapsedPercentage => recycledTween_cooldown.Tween.ElapsedPercentage();
 #endregion
 
 #region API
-	public void Start( float duration, bool loop = false, TweenCallback onCompleteDelegate = null )
-    {
-		isOngoing = true;
-		onComplete = onCompleteDelegate;
-		recycledTween_cooldown.Recycle( DOVirtual.DelayedCall( duration, OnComplete ).SetLoops( loop ? -1 : 1 ) );
-	}
+		public void Start( float duration, bool loop = false, TweenCallback onCompleteDelegate = null )
+		{
+			isOngoing = true;
+			onComplete = onCompleteDelegate;
+			recycledTween_cooldown.Recycle( DOVirtual.DelayedCall( duration, OnComplete ).SetLoops( loop ? -1 : 1 ) );
+		}
 
-	public void Start( float duration, string description, bool popupTextOnComplete, bool loop = false, TweenCallback onCompleteDelegate = null )
-	{
-		this.description = description;
-		this.popupTextOnComplete = popupTextOnComplete;
+		public void Start( float duration, string description, bool popupTextOnComplete, bool loop = false, TweenCallback onCompleteDelegate = null )
+		{
+			this.description = description;
+			this.popupTextOnComplete = popupTextOnComplete;
 
-		FFStudio.FFLogger.Log( "Cooldown duration for " + description + " started." );
-		FFLogger.PopUpText( Vector3.zero, "Cooldown duration for " + description + " started." );
+			FFStudio.FFLogger.Log( "Cooldown duration for " + description + " started." );
+			FFLogger.PopUpText( Vector3.zero, "Cooldown duration for " + description + " started." );
 
-		Start( duration, loop, onCompleteDelegate );
-	}
+			Start( duration, loop, onCompleteDelegate );
+		}
 
-	public void Kill()
-	{
-		recycledTween_cooldown.Kill();
-		isOngoing = false;
-	}
+		public void Kill()
+		{
+			recycledTween_cooldown.Kill();
+			isOngoing = false;
+		}
 #endregion
 
 #region Implementation
-    void OnComplete()
-    {
-		isOngoing = false;
-		onComplete?.Invoke();
-
-		if( popupTextOnComplete )
+		void OnComplete()
 		{
-            FFStudio.FFLogger.Log( "Cooldown duration for " + description + " is over." );
-			FFLogger.PopUpText( Vector3.zero, "Cooldown duration for " + description + " is over." );
+			isOngoing = false;
+			onComplete?.Invoke();
+
+			if( popupTextOnComplete )
+			{
+				FFStudio.FFLogger.Log( "Cooldown duration for " + description + " is over." );
+				FFLogger.PopUpText( Vector3.zero, "Cooldown duration for " + description + " is over." );
+			}
 		}
-	}
 #endregion
 
 #region Editor Only
 #if UNITY_EDITOR
 #endif
 #endregion
+	}
 }

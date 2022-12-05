808232c1a143592a5f201b7efc478f690a230bce
fauder_home <canik.burak@gmail.com>
2022-12-05 14:33:57 +0300

    Add Punch & Shake Tween wrapper classes for easier usage

---
+++ Assets/Script/FFStudio/Tween/PunchPositionTween.cs
@@ -0,0 +1,22 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using UnityEngine;
+using Sirenix.OdinInspector;
+using DG.Tweening;
+
+[ System.Serializable ]
+public class PunchPositionTween
+{
+	[ SuffixLabel( "units"   )           ] public Vector3 strength = Vector3.one;
+	[ SuffixLabel( "seconds" ), Min( 0 ) ] public float duration = 1;
+    [ SuffixLabel( "hz"      ), Min( 0 ) ] public int vibrato = 10;
+    [ Range( 0, 1 ) ] public float elasticity = 1;
+    public bool useSnapping = false;
+    public Ease ease = Ease.Linear;
+
+    public Tweener CreateTween( Transform transform )
+    {
+        return transform.DOPunchPosition( strength, duration, vibrato, elasticity, useSnapping )
+                        .SetEase( ease );
+    }
+}
---
+++ Assets/Script/FFStudio/Tween/ShakeRotationTween.cs
@@ -0,0 +1,22 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using UnityEngine;
+using Sirenix.OdinInspector;
+using DG.Tweening;
+
+[ System.Serializable ]
+public class ShakeRotationTween
+{
+	[ SuffixLabel( "units"   )           ] public Vector3 strength = Vector3.one;
+	[ SuffixLabel( "seconds" ), Min( 0 ) ] public float duration = 1;
+    [ SuffixLabel( "hz"      ), Min( 0 ) ] public int vibrato = 10;
+    [ Range( 0, 1 ) ] public float randomness = 1;
+	public bool fadeOut = true;
+	public Ease ease = Ease.Linear;
+
+    public Tweener CreateTween( Transform transform )
+    {
+        return transform.DOShakeRotation( duration, strength, vibrato, randomness, fadeOut )
+                        .SetEase( ease );
+    }
+}

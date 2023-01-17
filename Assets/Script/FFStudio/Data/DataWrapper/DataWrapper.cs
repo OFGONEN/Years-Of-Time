d3f61619ef85230723da6e633829642a656f5fff
ofgonen_office <ofgonen@gmail.com>
2023-01-17 20:15:51 +0300

    Move DataWrapper related script files into DataWrapper folder

--- Assets/Script/FFStudio/Data/DataWrapper/DataWrapper.cs
+++ Assets/Script/FFStudio/Data/DataWrapper/DataWrapper.cs
@@ -1,6 +1,7 @@
 /* Created by and for usage of FF Studios (2021). */
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace FFStudio

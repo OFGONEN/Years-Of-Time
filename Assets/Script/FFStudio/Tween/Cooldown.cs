2c6af72c988e67f0fcefdaeccce9f22f7c7fcca3
ofgonen-home <ofgonen@gmail.com>
2023-01-18 18:55:57 +0300

    🔃Cooldown: Change method signature
    - Wrap methods into Unity_Editor Symbol

--- Assets/Script/FFStudio/Tween/Cooldown.cs
+++ Assets/Script/FFStudio/Tween/Cooldown.cs
@@ -10,29 +10,29 @@ namespace FFStudio
 	{
 #region Fields
 		RecycledTween recycledTween_cooldown = new RecycledTween();
-
-		bool isOngoing = false;
-
 		TweenCallback onComplete;
 
+#if UNITY_EDITOR
 		bool popupTextOnComplete = false;
 		string description;
+#endif
 #endregion
 
 #region Properties
-		public bool IsOver => !isOngoing;
+		public bool IsPlaying => recycledTween_cooldown.IsPlaying();
 		public float ElapsedPercentage => recycledTween_cooldown.Tween.ElapsedPercentage();
 #endregion
 
 #region API
-		public void Start( float duration, bool loop = false, TweenCallback onCompleteDelegate = null )
+		public void Start( float duration, TweenCallback onCompleteDelegate = null, bool loop = false )
 		{
-			isOngoing = true;
 			onComplete = onCompleteDelegate;
 			recycledTween_cooldown.Recycle( DOVirtual.DelayedCall( duration, OnComplete ).SetLoops( loop ? -1 : 1 ) );
 		}
 
-		public void Start( float duration, string description, bool popupTextOnComplete, bool loop = false, TweenCallback onCompleteDelegate = null )
+#if UNITY_EDITOR
+		//! Do not use this on build code or wrap it inside of #if Unity_Editor
+		public void Start( float duration, string description, TweenCallback onCompleteDelegate = null, bool loop = false, bool popupTextOnComplete = false )
 		{
 			this.description = description;
 			this.popupTextOnComplete = popupTextOnComplete;
@@ -40,27 +40,28 @@ namespace FFStudio
 			FFStudio.FFLogger.Log( "Cooldown duration for " + description + " started." );
 			FFLogger.PopUpText( Vector3.zero, "Cooldown duration for " + description + " started." );
 
-			Start( duration, loop, onCompleteDelegate );
+			Start( duration,onCompleteDelegate, loop );
 		}
+#endif
 
 		public void Kill()
 		{
 			recycledTween_cooldown.Kill();
-			isOngoing = false;
 		}
 #endregion
 
 #region Implementation
 		void OnComplete()
 		{
-			isOngoing = false;
-			onComplete?.Invoke();
+			onComplete.Invoke();
 
+#if UNITY_EDITOR
 			if( popupTextOnComplete )
 			{
 				FFStudio.FFLogger.Log( "Cooldown duration for " + description + " is over." );
 				FFLogger.PopUpText( Vector3.zero, "Cooldown duration for " + description + " is over." );
 			}
+#endif
 		}
 #endregion
 

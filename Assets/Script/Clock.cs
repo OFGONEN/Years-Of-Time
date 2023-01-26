86adcff3af9a4eb0a08ceb7e007a096bef309981
ofgonen_office <ofgonen@gmail.com>
2023-01-26 14:28:48 +0300

    Implement Clock: Updating Visuals and Idle Position

---
+++ Assets/Script/Clock.cs
@@ -0,0 +1,55 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+using Sirenix.OdinInspector;
+
+public class Clock : MonoBehaviour
+{
+#region Fields
+  [ Title( "Shared Data" ) ]
+    [ SerializeField ] ClockData clock_data;
+
+  [ Title( "Components" ) ]
+    [ SerializeField ] MeshFilter clock_circle_meshFilter;
+    [ SerializeField ] Renderer clock_circle_renderer;
+    [ SerializeField ] MeshFilter clock_hand_second_meshFilter;
+    [ SerializeField ] Renderer clock_hand_second_renderer;
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+	[ Button() ]
+    public void SetIdlePosition( Vector3 position )
+    {
+		transform.position = position.SetY( GameSettings.Instance.clock_height_idle );
+	}
+
+    [ Button() ]
+    public void UpdateVisuals()
+    {
+		var circleSharedMaterials  = clock_circle_renderer.sharedMaterials;
+		circleSharedMaterials[ 1 ] = clock_data.ClockMaterial;
+
+		clock_circle_meshFilter.mesh              = clock_data.ClockCircleMesh;
+		clock_hand_second_meshFilter.mesh         = clock_data.ClockSecondHandMesh;
+		clock_circle_renderer.sharedMaterials     = circleSharedMaterials;
+		clock_hand_second_renderer.sharedMaterial = clock_data.ClockMaterial;
+	}
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}

860dcfba56d68c825cd784c046c15788eb393779
ofgonen-home <ofgonen@gmail.com>
2023-01-27 16:20:51 +0300

    ISlotEntity: SpawnClock

--- Assets/Script/ISlotEntity.cs
+++ Assets/Script/ISlotEntity.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public interface ISlotEntity
 {
+	void SpawnClock( int level );
 	Vector3 GetPosition();
 	bool IsClockPresent(); // Returns -1 if no clock is present
 	int CurrentClockLevel(); // Return current clock level

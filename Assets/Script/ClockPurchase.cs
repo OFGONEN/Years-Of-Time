ea66ae0c98e2a9f80093882ebf2402e6e20f438a
ofgonen-home <ofgonen@gmail.com>
2023-01-27 22:37:33 +0300

    ClockPurchase: Save & Load purchase count

--- Assets/Script/ClockPurchase.cs
+++ Assets/Script/ClockPurchase.cs
@@ -4,11 +4,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FFStudio;
+using Sirenix.OdinInspector;
 
 [ CreateAssetMenu( fileName = "notif_clock_purchase", menuName = "FF/Game/Clock Purchase" ) ]
-public class ClockPurchase : SharedFloatNotifier
+public class ClockPurchase : SharedBoolNotifier
 {
 #region Fields
+  [ Title( "Setup" ) ]
+    [ SerializeField ] float purchase_cost_base;
+
+  [ Title( "Shared" ) ]
+    [ SerializeField ] Currency currency;
+
+    
+    int purchase_count;
 #endregion
 
 #region Properties
@@ -18,9 +27,22 @@ public class ClockPurchase : SharedFloatNotifier
 #endregion
 
 #region API
+    public void LoadPurchaseCount( int defaultValue )
+    {
+		purchase_count = PlayerPrefsUtility.Instance.GetInt( ExtensionMethods.Key_ClockPurchaseCount, defaultValue );
+	}
+
+    public void SavePurchaseCount()
+    {
+        PlayerPrefsUtility.Instance.SetInt( ExtensionMethods.Key_ClockPurchaseCount, purchase_count );
+    }
 #endregion
 
 #region Implementation
+    float GetClockPurchaseCost()
+    {
+        return purchase_cost_base + Mathf.Pow( purchase_count, 1.25f ) - purchase_count;
+    }
 #endregion
 
 #region Editor Only

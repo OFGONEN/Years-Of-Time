0d07415c5d0454b174b5cae9ab66356aeb96a390
ofgonen-home <ofgonen@gmail.com>
2023-01-27 22:14:48 +0300

    Clock Purhcase: Create & Configure

---
+++ Assets/Script/ClockPurchase.cs
@@ -0,0 +1,30 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+
+[ CreateAssetMenu( fileName = "notif_clock_purchase", menuName = "FF/Game/Clock Purchase" ) ]
+public class ClockPurchase : SharedFloatNotifier
+{
+#region Fields
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}

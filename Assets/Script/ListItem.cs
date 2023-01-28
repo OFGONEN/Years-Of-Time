befb89eb37db65d351d271c4c7a65621d7698350
ofgonen-home <ofgonen@gmail.com>
2023-01-29 00:37:31 +0300

    Item: Create Script file & Implement Item RunTimeSet

---
+++ Assets/Script/ListItem.cs
@@ -0,0 +1,30 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+
+[ CreateAssetMenu( fileName = "list_item", menuName = "FF/Game/List Item" ) ]
+public class ListItem : RuntimeSet< Vector2Int, Item >
+{
+#region Fields
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}

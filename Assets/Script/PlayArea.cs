badb59cb668deeb4b5e6636d41c90bb11ad08cde
ofgonen-home <ofgonen@gmail.com>
2023-01-30 01:12:37 +0300

    Play Area: Change & Tween Size

--- Assets/Script/PlayArea.cs
+++ Assets/Script/PlayArea.cs
@@ -3,22 +3,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FFStudio;
+using DG.Tweening;
 
 public class PlayArea : MonoBehaviour
 {
 #region Fields
+    [ SerializeField ] SharedIntNotifier notif_item_unlock_index;
+
+    RecycledTween recycledTween = new RecycledTween();
 #endregion
 
 #region Properties
 #endregion
 
 #region Unity API
+    private void Start()
+    {
+		ChangeSize();
+	}
 #endregion
 
 #region API
+    public void OnItemUnlocked()
+    {
+		notif_item_unlock_index.SharedValue = Mathf.Min( notif_item_unlock_index.sharedValue + 1, GameSettings.Instance.playArea_size_array.Length - 1 );
+
+		TweenSize();
+	}
 #endregion
 
 #region Implementation
+    void ChangeSize()
+    {
+		var size = GameSettings.Instance.playArea_size_array[ notif_item_unlock_index.sharedValue ];
+		transform.localScale = Vector3.one.SetX( size ).SetZ( size );
+    }
+
+    void TweenSize()
+    {
+		var size = GameSettings.Instance.playArea_size_array[ notif_item_unlock_index.sharedValue ];
+
+		recycledTween.Recycle( transform.DOScale( size, GameSettings.Instance.playArea_size_duration )
+			.SetEase( GameSettings.Instance.playArea_size_ease ) );
+	}
 #endregion
 
 #region Editor Only

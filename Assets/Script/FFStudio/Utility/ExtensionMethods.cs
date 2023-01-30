30dbfd1c9e59b7a50cc45695ccdd9e94bc1f1002
ofgonen-home <ofgonen@gmail.com>
2023-01-30 16:46:21 +0300

    ExtensionMethods: Key_Income

--- Assets/Script/FFStudio/Utility/ExtensionMethods.cs
+++ Assets/Script/FFStudio/Utility/ExtensionMethods.cs
@@ -13,6 +13,7 @@ namespace FFStudio
 	{
 		public static readonly string SAVE_PATH = Application.persistentDataPath + "/Saves/";
 		public static readonly string Key_ClockPurchaseCount = "clock_purchase_count";
+		public static readonly string Key_Income = "income_index";
 
 		static List< Transform > baseModelBones   = new List< Transform >( 96 );
 		static List< Transform > targetModelBones = new List< Transform >( 96 );

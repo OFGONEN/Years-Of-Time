79a5d1aa82be2c4a7e7f3d7686303d19a8ceef44
ofgonen-home <ofgonen@gmail.com>
2023-01-30 16:48:05 +0300

    IncomeCofactor: Create IncomeData

--- Assets/Script/IncomeCofactor.cs
+++ Assets/Script/IncomeCofactor.cs
@@ -4,20 +4,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FFStudio;
+using Sirenix.OdinInspector;
 
 [ CreateAssetMenu( fileName = "notif_income_cofactor", menuName = "FF/Game/Income Cofactor" ) ]
 public class IncomeCofactor : SharedFloatNotifier
 {
 #region Fields
+  [ Title( "Setup" ) ]
+    [ SerializeField ] IncomeData[] income_data_array;
+
+  [ Title( "Shared" ) ]
+    [ SerializeField ] Currency notif_currency;
+    int income_index;
 #endregion
 
 #region Properties
+    public float IncomeCost     => income_data_array[ income_index ].income_cost;
+    public float IncomeValue    => income_data_array[ income_index ].income_value;
+    public float IncomeDataSize => income_data_array.Length;
 #endregion
 
 #region Unity API
 #endregion
 
 #region API
+    public void LoadIncomeCofactor()
+    {
+		income_index = PlayerPrefsUtility.Instance.GetInt( ExtensionMethods.Key_Income, 0 );
+		SharedValue  = IncomeValue;
+	}
+
+    public void UnlockIncome()
+    {
+		notif_currency.SharedValue -= IncomeCost;
+
+		income_index++;
+		PlayerPrefsUtility.Instance.SetInt( ExtensionMethods.Key_Income, income_index );
+		SharedValue = IncomeValue;
+	}
 #endregion
 
 #region Implementation
@@ -27,4 +51,11 @@ public class IncomeCofactor : SharedFloatNotifier
 #if UNITY_EDITOR
 #endif
 #endregion
+}
+
+[ System.Serializable ]
+public struct IncomeData
+{
+	public float income_cost;
+	public float income_value;
 }

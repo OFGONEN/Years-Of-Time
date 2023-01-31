531c562d3a56889b85424d2e2fd6ddf9d84ae449
ofgonen-home <ofgonen@gmail.com>
2023-01-31 17:57:31 +0300

    Tutorial Phase 1

--- Assets/Script/Tutorial.cs
+++ Assets/Script/Tutorial.cs
@@ -5,10 +5,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using FFStudio;
 using Sirenix.OdinInspector;
+using TMPro;
+using DG.Tweening;
 
 public class Tutorial : MonoBehaviour
 {
 #region Fields
+  [ Title( "Setup" ) ]
+    [ SerializeField ] RectTransform transform_hand;
+    [ SerializeField ] RectTransform transform_mask;
+    [ SerializeField ] RectTransform transform_information;
+    [ SerializeField ] TextMeshProUGUI textRenderer_information;
+
+  [ Title( "Settings" ) ]
+    [ SerializeField ] float hand_click_scale;
+    [ SerializeField ] float hand_click_cooldown;
+    
+    [ FoldoutGroup( "Phase 1" ), SerializeField ] RectTransform target_hand;
+
+    RecycledSequence recycledSequence = new RecycledSequence();
 #endregion
 
 #region Properties
@@ -18,9 +33,41 @@ public class Tutorial : MonoBehaviour
 #endregion
 
 #region API
+    [ Button() ]
+    public void StartPhaseOne()
+    {
+		transform_hand.position = target_hand.position;
+		transform_hand.rotation = target_hand.rotation;
+
+		transform_hand.gameObject.SetActive( true );
+		transform_mask.gameObject.SetActive( true );
+		transform_information.gameObject.SetActive( true );
+
+		DoHandClickAnimation();
+	}
 #endregion
 
 #region Implementation
+    void DoHandClickAnimation()
+    {
+
+		var sequence = recycledSequence.Recycle( DoHandClickAnimation );
+
+		sequence.AppendCallback( SetHandClickScale );
+		sequence.AppendInterval( hand_click_cooldown );
+		sequence.AppendCallback( SetHandDefaultScale );
+		sequence.AppendInterval( hand_click_cooldown );
+	}
+
+    void SetHandDefaultScale()
+    {
+		transform_hand.localScale = Vector3.one;
+	}
+
+    void SetHandClickScale()
+    {
+		transform_hand.localScale = Vector3.one * hand_click_scale;
+	}
 #endregion
 
 #region Editor Only
